15d3f7248317ea625e68a6a901fb295f30988cd9
Riley <riley@rileyhtaylor.com>
2025-04-19 01:12:45 -0500

    added argpase and yaml. Got argparse setup and a simple printout in color! Now we labor.. just tomorrow.

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 #Initial Commit!
+
+from colorama import *
+import argparse
+from yaml import *
+
+init(autoreset=True) #reset color values after each print.
+
+def parser_args():
+    parser = argparse.ArgumentParser(description="RHTELANT - RHT'S ELA Network Tool")
+    parser.add_argument("--target", required=True, help="Target IP address")
+    parser.add_argument("--port", required=True, help="Target port")
+    return parser.parse_args()
+
+def main():
+    arguments = parser_args()
+    print(Fore.CYAN + "Hello!")
+
+
+
+if __name__ == "__main__":
+    main()
